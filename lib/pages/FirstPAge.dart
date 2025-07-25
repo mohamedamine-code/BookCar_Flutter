fe3c0b08e0ed3edc4b26c80b97ae0a65eea48da1
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-07-25 18:23:27 +0100

    UI commit

--- lib/pages/FirstPAge.dart
+++ lib/pages/FirstPAge.dart
@@ -7,46 +7,76 @@ class Firstpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            flex: 3,
-            child: Image.asset("assets/img/0a12131d595ca1ac8625fc43a5a87443.jpg",fit: BoxFit.cover,)),
-            Expanded(
-              flex: 1,
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 255, 178, 63)
+          Image.asset(
+            "assets/img/On the way-cuate (2).png",
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            left: 0,
+            right: 0, // Add this!
+            top: MediaQuery.of(context).size.height - 250,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: MediaQuery.of(context).size.width,
+              height: 250,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    const Color.fromARGB(209, 11, 11, 11),
+                    const Color.fromARGB(200, 11, 11, 11),
+                    const Color.fromARGB(184, 11, 11, 11),
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10,),
+                  Text(
+                    "Welcome To GoCab",
+                    style: TextStyle(fontSize: 37, fontWeight: FontWeight.bold,color: Colors.white),
                   ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Welcome To GoCab",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-                      Text("Ride Any Time ✌️!",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
-                      SizedBox(height: 25,),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: GestureDetector(
-                          onTap: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: Colors.orange,
-                              borderRadius: BorderRadius.circular(12)
-                            ),
-                            child: Text("Let's Go !",style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
+                  Text(
+                    "Ride Any Time ✌️!",
+                    style: TextStyle(fontSize: 37, fontWeight: FontWeight.bold,color: Colors.white),
+                  ),
+                  SizedBox(height: 25),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Text(
+                          "Let's Go !",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
-                      )
-                    ],
+                      ),
+                    ),
                   ),
-                ),
-              ))
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
